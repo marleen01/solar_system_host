44a3b7ebf2f103113dfd9640d52b7341b12766d2
unknown <ylmz.emre2000@gmail.com>
2020-07-30 03:32:58 +0300

    enhanced graphics

--- the-solar-system/planet.js
+++ the-solar-system/planet.js
@@ -1,9 +1,10 @@
 class Planet {
-  constructor(diameter, distance, orbitspeed, angle) {
+  constructor(diameter, distance, orbitspeed, angle, t = "planet") {
     this.diameter = diameter;
     this.distance = distance;
     this.orbitspeed = orbitspeed;
     this.angle = angle;
+    this.t = t;
   }
 
   orbit() {
@@ -12,11 +13,17 @@ class Planet {
 
   show() {
     push();
-    fill(255);
     rotate(this.angle);
     translate(this.distance, 0);
+    if (this.t === "planet") {
+      fill(0, 0, 0, 150);
+      rotate(-HALF_PI);
+      rect(-this.diameter / 2, 0, this.diameter, 400);
+    }
+    fill(255);
     point(0, 0);
     ellipse(0, 0, this.diameter);
     pop();
+
   }
 }
--- the-solar-system/sketch.js
+++ the-solar-system/sketch.js
@@ -4,11 +4,12 @@
 let planets = [];
 let dia = 10;
 let dis = 100;
-let orb = 0.01;
+let orb = 0.002;
 
 function setup() {
-	createCanvas(600, 600);
-	sun = new Planet(40, 0, 0, 100);
+	// frameRate(60);
+	createCanvas(1000, 1000);
+	sun = new Planet(40, 0, 0, 100, "star");
 	mercury = new Planet(dia*0.383, dis*0.387, orb*1.59, random(TWO_PI));
 	venus = new Planet(dia*0.949, dis*0.723, orb*1.18, random(TWO_PI));
 	earth = new Planet(dia, dis, orb, random(TWO_PI));
@@ -19,12 +20,24 @@ function setup() {
 }
 
 function draw() {
-	background(0,0,20);
+	background(0);
 	translate(width/2, height/2);
 	sun.show();
 	sun.orbit();
+
+	for (var l = 1; l < 21; l++) {
+		noFill();
+		strokeWeight(15);
+		stroke(255, 255, 255, map(21 - l, 0, 21, 0, 21));
+		circle(0, 0, sun.diameter + l * 30 - 14);
+	}
 	for(let i = 0; i < planets.length; i++) {
+		noStroke();
 		planets[i].show();
 		planets[i].orbit();
+
+
+
 	}
+	// noLoop();
 }
